b0f6557da76da788dcfd5c53c40b3cac4ce37e84
Sebastian <norlagtuto@gmail.com>
2021-12-04 13:09:02 +0100

    finished 5.14

---
+++ EX_5_14/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+bool contient(int first_tab[], size_t first_tab_size, int second_tab[], size_t second_tab_size);
+bool egalite(int first_tab[], size_t first_tab_size, int second_tab[], size_t second_tab_size);
+
+int main()
+{
+	int tab1[] = {3, 3, 1, 1, 2, 1};	
+	int tab2[] = {1, 2, 3};
+
+	cout << egalite(tab1, 6, tab2, 3) << endl;
+	cout << egalite(tab2, 3, tab1, 6) << endl;
+
+	return EXIT_SUCCESS;
+}
+
+
+bool egalite(int first_tab[], size_t first_tab_size, int second_tab[], size_t second_tab_size)
+{
+	int* smaller_tab;
+	size_t smaller_tab_size;
+	int* bigger_tab;
+	size_t bigger_tab_size;
+
+	if(first_tab_size > second_tab_size)
+	{
+		smaller_tab = second_tab;
+		smaller_tab_size = second_tab_size;
+		bigger_tab = first_tab;
+		bigger_tab_size = first_tab_size;
+	}
+	else
+	{
+		smaller_tab = first_tab;
+		smaller_tab_size = first_tab_size;
+		bigger_tab = second_tab;
+		bigger_tab_size = second_tab_size;
+	}
+
+	return ((first_tab_size == 0 && second_tab_size == 0) ||
+		(contient(smaller_tab, smaller_tab_size, bigger_tab, bigger_tab_size) && 
+		contient(bigger_tab, bigger_tab_size, smaller_tab, smaller_tab_size)));	
+}
+
+bool contient(int first_tab[], size_t first_tab_size, int second_tab[], size_t second_tab_size)
+{
+	bool found;
+
+	for(int i = 0; i < first_tab_size; ++i)
+	{
+		found = false;
+		for(int j = 0; j < second_tab_size; ++j)
+		{
+			if(first_tab[i] == second_tab[j])
+			{
+				cout << "Found " << first_tab[i] << endl;
+				found = true;
+				break;
+			}
+		}
+
+		if(!found)
+			return false;
+	}
+	return true;
+}
--- ExercicesCpp/main.cpp
+++ ExercicesCpp/main.cpp
@@ -13,32 +13,31 @@
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-   int t[] = {1, 2, 3, 4, 5, 4, 3, 2, 1, 0};
-   //for (int i = 1; i < 10; ++i) { t[i] = t[i - 1]; } // 1 1 1 1 1 1 1 1 1 1
-
-   //for (int i = 9; i > 0; --i) { t[i] = t[i - 1]; } // 1 1 2 3 4 5 4 3 2 1
-
-   //for (int i = 0; i < 9; ++i) { t[i] = t[i + 1]; } // 2 3 4 5 4 3 2 1 0 0
-
-   //for (int i = 8; i >= 0; --i) { t[i] = t[i + 1]; } // 0 0 0 0 0 0 0 0 0 0
-
-   //for (int i = 1; i < 10; ++i) { t[i] = t[i] + t[i - 1]; } // 1 3 6 10 15 19 22 24 25 25
-
-   //for (int i = 1; i < 10; i = i + 2) { t[i] = 0; } // 1 0 3 0 5 0 3 0 1 0
-
-   //for (int i = 0; i < 5; ++i) { t[i + 5] = t[i]; } // 1 2 3 4 5 1 2 3 4 5
-
-   for (int i = 1; i < 5; ++i) { t[i] = t[9 - i]; } // 1 1 2 3 4 4 3 2 1 0
-
-   for (int i = 0; i < 10; ++i) {
-      cout << t[i] << " ";
+void afficher(const vector<int>& v) {
+   cout << "[ ";
+   for (auto i = v.begin(); i != v.end(); ++i) {
+      if (i != v.begin()) cout << ", ";
+      cout << *i;
    }
-   cout << endl;
-
-	return EXIT_SUCCESS;
+   cout << " ]";
 }
 
+void comparer(const vector<int>& v1, const vector<int>& v2) {
+   afficher(v1);
+   cout << (v1 == v2 ? " = " : v1 < v2 ? " < " : " > ");
+   afficher(v2);
+   cout << endl;
+}
+
+int main() {
+   comparer({1, 2, 3}, {1, 2, 3});
+   comparer({1, 1, 3}, {1, 2, 3});
+   comparer({1, 3, 1}, {1, 2, 3});
+   comparer({1, 2, 3, 4}, {1, 2, 3});
+   comparer({1, 2, 2, 4}, {1, 2, 3});
+   comparer({1, 2, 4, 4}, {1, 2, 3});
+}
